d909d5451c832caae0e09791e0f1535ff92efdd0
Aleksander <aleksander.kunup@gmail.com>
2018-05-09 23:21:43 +0300

    change front

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+app
+.use('/media', express.static(__dirname + '/media'))
+.use(express.static(__dirname + '/public'))
+
+
+//.use(express.static(path.join(__dirname, 'public')))
+  //.set('views', path.join(__dirname, 'views'))
+  //.set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
